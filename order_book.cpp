9038c25626a31d667ad8ab36532e72f05e36efc4
Tan Lip Wei <53000252+lipwei1808@users.noreply.github.com>
2024-03-21 13:36:13 +0800

    Add formatting and cancel command interface

--- order_book.cpp
+++ order_book.cpp
@@ -1,12 +1,12 @@
-#include <assert.h>
+#include <algorithm>
+#include <iostream>
 #include <memory>
 #include <mutex>
-#include <iostream>
-#include <algorithm>
 #include <string>
+#include <assert.h>
 
-#include "order_book.hpp"
 #include "order.hpp"
+#include "order_book.hpp"
 #include "price.hpp"
 
 template void OrderBook::Handle<Side::BUY>(std::shared_ptr<Order> order);
@@ -21,49 +21,49 @@ template std::shared_ptr<Price> OrderBook::GetPrice<Side::BUY>(price_t price);
 template <Side side>
 void OrderBook::Handle(std::shared_ptr<Order> order)
 {
-  assert(side == Side::SELL || side == Side::BUY);
-  assert(order->GetSide() == side);
-  assert(order->GetActivated() == false);
-  std::unique_lock<std::mutex> l(order_book_lock);
+    assert(side == Side::SELL || side == Side::BUY);
+    assert(order->GetSide() == side);
+    assert(order->GetActivated() == false);
+    std::unique_lock<std::mutex> l(order_book_lock);
 
-  // Get timestamp for order
-  order->SetTimestamp(getCurrentTimestamp());
+    // Get timestamp for order
+    order->SetTimestamp(getCurrentTimestamp());
 
-  // Insert dummy node into order
-  if constexpr (side == Side::BUY)
-    Add<Side::BUY>(order);
-  else
-    Add<Side::SELL>(order);
+    // Insert dummy node into order
+    if constexpr (side == Side::BUY)
+        Add<Side::BUY>(order);
+    else
+        Add<Side::SELL>(order);
 
-  l.unlock();
+    l.unlock();
 
-  // Execute
-  bool filled = Execute<side>(order);
-  if (!filled)
-  {
-    Output::OrderAdded(
-        order->GetOrderId(),
-        order->GetInstrumentId().c_str(),
-        order->GetPrice(),
-        order->GetCount(),
-        side == Side::SELL,
-        order->GetTimestamp());
-  }
+    // Execute
+    bool filled = Execute<side>(order);
+    if (!filled)
+    {
+        Output::OrderAdded(
+            order->GetOrderId(),
+            order->GetInstrumentId().c_str(),
+            order->GetPrice(),
+            order->GetCount(),
+            side == Side::SELL,
+            order->GetTimestamp());
+    }
 
-  // Add
-  order->Activate();
+    // Add
+    order->Activate();
 }
 
 template <Side side>
 void OrderBook::Add(std::shared_ptr<Order> order)
 {
-  assert(order->GetSide() == side);
-  if constexpr (side == Side::BUY)
-    std::unique_lock<std::mutex> l(bids_lock);
-  else
-    std::unique_lock<std::mutex> l(asks_lock);
-  std::shared_ptr<Price> p = GetPrice<side>(order->GetPrice());
-  p->AddOrder(order);
+    assert(order->GetSide() == side);
+    if constexpr (side == Side::BUY)
+        std::unique_lock<std::mutex> l(bids_lock);
+    else
+        std::unique_lock<std::mutex> l(asks_lock);
+    std::shared_ptr<Price> p = GetPrice<side>(order->GetPrice());
+    p->AddOrder(order);
 }
 
 /**
@@ -73,135 +73,128 @@ void OrderBook::Add(std::shared_ptr<Order> order)
 template <Side side>
 bool OrderBook::Execute(std::shared_ptr<Order> order)
 {
-  assert(side == Side::SELL || side == Side::BUY);
-  assert(order->GetSide() == side);
-  assert(order->GetActivated() == false);
-  std::unique_lock<std::mutex> l;
-  if constexpr (side == Side::BUY)
-    l = std::unique_lock<std::mutex>(asks_lock);
-  else
-    l = std::unique_lock<std::mutex>(bids_lock);
-
-  while (order->GetCount() > 0)
-  {
-    // Check if any sell orders
+    assert(side == Side::SELL || side == Side::BUY);
+    assert(order->GetSide() == side);
+    assert(order->GetActivated() == false);
+    std::unique_lock<std::mutex> l;
     if constexpr (side == Side::BUY)
-    {
-      if (asks.Size() == 0)
-        return false;
-    }
+        l = std::unique_lock<std::mutex>(asks_lock);
     else
+        l = std::unique_lock<std::mutex>(bids_lock);
+
+    while (order->GetCount() > 0)
     {
-      if (bids.Size() == 0)
-        return false;
-    }
+        // Check if any sell orders
+        if constexpr (side == Side::BUY)
+        {
+            if (asks.Size() == 0)
+                return false;
+        }
+        else
+        {
+            if (bids.Size() == 0)
+                return false;
+        }
 
-    // Check if lowest sell order can match the buy
-    auto firstEl = ([&]()
-                    {
-        if constexpr (side == Side::BUY) 
-          return asks.begin();
-        else 
-          return bids.begin(); })();
-    price_t price = firstEl->first;
-    assert(firstEl->second.initialised);
-    std::shared_ptr<Price> priceQueue = firstEl->second.Get();
-    assert(priceQueue->Size() != 0);
+        // Check if lowest sell order can match the buy
+        auto firstEl = ([&]() {
+          if constexpr (side == Side::BUY) 
+            return asks.begin();
+          else 
+            return bids.begin(); 
+        })();
+        price_t price = firstEl->first;
+        assert(firstEl->second.initialised);
+        std::shared_ptr<Price> priceQueue = firstEl->second.Get();
+        assert(priceQueue->Size() != 0);
 
-    if constexpr (side == Side::BUY)
-    {
-      if (price > order->GetPrice())
-        return false;
-    }
-    else
-    {
-      if (price < order->GetPrice())
-        return false;
-    }
+        if constexpr (side == Side::BUY)
+        {
+            if (price > order->GetPrice())
+                return false;
+        }
+        else
+        {
+            if (price < order->GetPrice())
+                return false;
+        }
 
-    // Iteratively match with all orders in this price queue.
-    while (order->GetCount() > 0 && priceQueue->Size())
-    {
-      std::shared_ptr<Order> oppOrder = priceQueue->Front();
-      // Check if first order's timestamp comes before the current buy
-      if (oppOrder->GetTimestamp() > order->GetTimestamp())
-        break;
+        // Iteratively match with all orders in this price queue.
+        while (order->GetCount() > 0 && priceQueue->Size())
+        {
+            std::shared_ptr<Order> oppOrder = priceQueue->Front();
+            // Check if first order's timestamp comes before the current buy
+            if (oppOrder->GetTimestamp() > order->GetTimestamp())
+                break;
 
-      // Check if sell order is activated
-      while (!oppOrder->GetActivated())
-        oppOrder->cv.wait(l);
+            // Check if sell order is activated
+            while (!oppOrder->GetActivated())
+                oppOrder->cv.wait(l);
 
-      // Check if dummy order has already been filled
-      if (oppOrder->GetCount() == 0)
-      {
-        priceQueue->Pop();
-        continue;
-      }
-      oppOrder->IncrementExecutionId();
-      MatchOrders(order, oppOrder);
-      if (oppOrder->GetCount() == 0)
-      {
-        priceQueue->Pop();
-      }
-    }
+            // Check if dummy order has already been filled
+            if (oppOrder->GetCount() == 0)
+            {
+                priceQueue->Pop();
+                continue;
+            }
+            oppOrder->IncrementExecutionId();
+            MatchOrders(order, oppOrder);
+            if (oppOrder->GetCount() == 0)
+                priceQueue->Pop();
+        }
 
-    if (priceQueue->Size() == 0)
-    {
-      // Remove from map of prices
-      size_t num = ([&]() -> size_t
+        if (priceQueue->Size() == 0)
+        {
+            // Remove from map of prices
+            size_t num = ([&]() -> size_t
                     {
           if constexpr (side == Side::BUY) 
             return asks.Erase(firstEl->first);
           else 
             return bids.Erase(firstEl->first); })();
-      assert(num == 1);
+            assert(num == 1);
+        }
     }
-  }
-  return order->GetCount() == 0;
+    return order->GetCount() == 0;
 }
 
 void OrderBook::MatchOrders(std::shared_ptr<Order> incoming, std::shared_ptr<Order> resting)
 {
-  unsigned int qty = std::min(incoming->GetCount(), resting->GetCount());
-  if (resting->GetCount() == incoming->GetCount())
-  {
-    incoming->Fill();
-    resting->Fill();
-  }
-  else if (resting->GetCount() > incoming->GetCount())
-  {
-    resting->Fill(incoming->GetCount());
-    incoming->Fill();
-  }
-  else
-  {
-    incoming->Fill(resting->GetCount());
-    resting->Fill();
-  }
-  Output::OrderExecuted(
-      resting->GetOrderId(),
-      incoming->GetOrderId(),
-      resting->GetExecutionId(),
-      resting->GetPrice(),
-      qty,
-      getCurrentTimestamp());
+    unsigned int qty = std::min(incoming->GetCount(), resting->GetCount());
+    if (resting->GetCount() == incoming->GetCount())
+    {
+        incoming->Fill();
+        resting->Fill();
+    }
+    else if (resting->GetCount() > incoming->GetCount())
+    {
+        resting->Fill(incoming->GetCount());
+        incoming->Fill();
+    }
+    else
+    {
+        incoming->Fill(resting->GetCount());
+        resting->Fill();
+    }
+    Output::OrderExecuted(
+        resting->GetOrderId(), incoming->GetOrderId(), resting->GetExecutionId(), resting->GetPrice(), qty, getCurrentTimestamp());
 }
 
 template <Side side>
 std::shared_ptr<Price> OrderBook::GetPrice(price_t price)
 {
-  WrapperValue<std::shared_ptr<Price>> &w = ([&]()
-                                             {
+    WrapperValue<std::shared_ptr<Price>> &w = ([&]() {
       if constexpr (side == Side::BUY)
         return std::ref(bids.Get(price));
       else
-        return std::ref(asks.Get(price)); })();
+        return std::ref(asks.Get(price)); 
+    })();
 
-  std::unique_lock<std::mutex> l(w.lock);
-  if (!w.initialised)
-  {
-    w.initialised = true;
-    w.val = std::make_shared<Price>();
-  }
-  return w.val;
+    std::unique_lock<std::mutex> l(w.lock);
+    if (!w.initialised)
+    {
+        w.initialised = true;
+        w.val = std::make_shared<Price>();
+    }
+    return w.val;
 }
--- order_book.hpp
+++ order_book.hpp
@@ -1,9 +1,9 @@
 #ifndef ORDER_BOOK_HPP
 #define ORDER_BOOK_HPP
 
-#include <mutex>
 #include <chrono>
 #include <memory>
+#include <mutex>
 #include <assert.h>
 
 #include "atomic_map.hpp"
@@ -12,32 +12,43 @@
 
 inline std::chrono::microseconds::rep getCurrentTimestamp() noexcept
 {
-  return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
 }
 
 // TODO: Check if bids_lock/asks_lock required when using AtomicMap
 class OrderBook
 {
 public:
-  OrderBook() = default;
-  template <Side side>
-  void Handle(std::shared_ptr<Order> order);
+    OrderBook() = default;
+    template <Side side>
+    void Handle(std::shared_ptr<Order> order);
+    template <Side side>
+    void Cancel(std::shared_ptr<Order> order)
+    {
+        assert(order->GetSide() == side);
+        if constexpr (side == Side::BUY)
+            l = std::unique_lock<std::mutex>(bids_lock);
+        else
+            l = std::unique_lock<std::mutex>(asks_lock);
+
+        std::shared_ptr<Price> priceLevel = GetPrice(order->GetPrice());
+    }
 
 private:
-  template <Side side>
-  void Add(std::shared_ptr<Order> order);
-  template <Side side>
-  std::shared_ptr<Price> GetPrice(price_t price);
-  template <Side side>
-  bool Execute(std::shared_ptr<Order> order);
-  void MatchOrders(std::shared_ptr<Order> o1, std::shared_ptr<Order> o2);
+    template <Side side>
+    void Add(std::shared_ptr<Order> order);
+    template <Side side>
+    std::shared_ptr<Price> GetPrice(price_t price);
+    template <Side side>
+    bool Execute(std::shared_ptr<Order> order);
+    void MatchOrders(std::shared_ptr<Order> o1, std::shared_ptr<Order> o2);
 
-  AtomicMap<price_t, WrapperValue<std::shared_ptr<Price>>, std::greater<price_t>> bids;
-  AtomicMap<price_t, WrapperValue<std::shared_ptr<Price>>> asks;
+    AtomicMap<price_t, WrapperValue<std::shared_ptr<Price>>, std::greater<price_t>> bids;
+    AtomicMap<price_t, WrapperValue<std::shared_ptr<Price>>> asks;
 
-  std::mutex order_book_lock;
-  std::mutex bids_lock;
-  std::mutex asks_lock;
+    std::mutex order_book_lock;
+    std::mutex bids_lock;
+    std::mutex asks_lock;
 };
 
 #endif
