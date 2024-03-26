4a066f9d74d8222ff6f75d0e5a4e6cbec7072106
lipwei.tan <lipwei.tan@bytedance.com>
2024-03-26 10:39:49 +0800

    grade seq script

--- grade-seq.sh
+++ grade-seq.sh
@@ -2,16 +2,21 @@
 
 for i in ./test_cases/seq/*.in; do
   # Run server as a background process
-  ./build/engine 3000 > output.txt &
+  ./build/engine 3000 > output.txt 2> /dev/null &
 
   # Get server pid
   server_pid=$!
 
-  ./build/mygrader $i
+  # Get basename of file
+  file_name=$(basename "$i")
+  base_name="${file_name%.*}"
+
+  ./build/mygrader $i > /dev/null
   # awk '{ for(i=1; i<NF; i++) printf "%s ", $i; print "" }' "$i"
-  echo =========
-  cat output.txt
-  echo =============
+  diff <(awk '{ for(i=1; i<NF; i++) printf "%s ", $i; print "" }' output.txt) <(awk '{ for(i=1; i<NF; i++) printf "%s ", $i; print "" }' ./test_cases/seq/$base_name.out)
+  if [[ $? != 0 ]]; then
+    echo $i failed
+  fi
   rm output.txt
   kill "$server_pid"
 done
